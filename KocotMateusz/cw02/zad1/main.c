a9296f3eb33d00b514f2957c9df9e315fc4da343
Mateusz Kocot <mateuszkocot99@gmail.com>
2020-03-19 21:32:13 +0100

    cw02 finished

--- KocotMateusz/cw02/zad1/main.c
+++ KocotMateusz/cw02/zad1/main.c
@@ -1,8 +1,20 @@
-#include "iolib.h"
-#include "iolib.c"
+#include "iooperations.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <sys/times.h>
+#include <unistd.h>
+
+double calTime(clock_t start, clock_t end)
+{
+    return ((double) (end - start)) / sysconf(_SC_CLK_TCK);
+}
+
+void printTime(double stime, double utime)
+{
+    printf("s: %f, u: %f\n", stime, utime);
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,23 +23,26 @@ int main(int argc, char *argv[])
         printf("You have to choose either generate, sort or copy!\n");
         return 1;
     }
+    struct tms *tstart = calloc(1, sizeof(struct tms)), *tend = calloc(1, sizeof(struct tms));
+    times(tstart);
     if (strcmp(argv[1], "generate") == 0)
     {
         if (argc != 5)
         {
-            printf("Wrong arguments number!\n");
-            return 1;
+            printf("Wrong! Use ./main generate [fileName] [recNum] [recLen] instead!\n");
         }
-        generate(argv[2], atoi(argv[3]), atoi(argv[4]));
+        else
+        {
+            generate(argv[2], atoi(argv[3]), atoi(argv[4]));
+        }
     }
     else if (strcmp(argv[1], "sort") == 0)
     {
         if (argc != 6)
         {
-            printf("Wrong arguments number!\n");
-            return 1;
+            printf("Wrong! Use ./main sort [fileName] [recNum] [recLen] [sys/lib] instead!\n");
         }
-        if (strcmp(argv[5], "sys") == 0)
+        else if (strcmp(argv[5], "sys") == 0)
         {
             sysSort(argv[2], atoi(argv[3]), atoi(argv[4]));
         }
@@ -38,17 +53,15 @@ int main(int argc, char *argv[])
         else
         {
             printf("Fifth argument should be either sys or lib!\n");
-            return 1;
         }
     }
     else if (strcmp(argv[1], "copy") == 0)
     {
         if (argc != 7)
         {
-            printf("Wrong arguments number!\n");
-            return 1;
+            printf("Wrong! Use ./main copy [fileFrom] [fileTo] [recNum] [recLen] [sys/lib] instead!\n");
         }
-        if (strcmp(argv[6], "sys") == 0)
+        else if (strcmp(argv[6], "sys") == 0)
         {
             sysCopy(argv[2], argv[3], atoi(argv[4]), atoi(argv[5]));
         }
@@ -59,13 +72,13 @@ int main(int argc, char *argv[])
         else
         {
             printf("Sixth argument should be either sys or lib!\n");
-            return 1;
         }
     }
     else
     {
         printf("Wrong argument!\n");
-        return 1;
     }
-    return 0;
+    times(tend);
+    printTime(calTime(tstart->tms_stime, tend->tms_stime), calTime(tstart->tms_utime, tend->tms_utime));
+    free(tstart), free(tend);
 }
